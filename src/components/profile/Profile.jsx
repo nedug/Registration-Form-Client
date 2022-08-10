e8b4988e7d49e23194bf22ccacbc55873d52029a
Alexander R <ru5nedug@mail.ru>
2022-08-10 12:52:28 +0300

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -11,11 +11,11 @@ const Profile = () => {
     return (
         <div className='flex justify-center'>
 
-            <div className='flex flex-col items-center bg-gray-200 shadow-xl hover:bg-gray-200 transition-all my-10 h-[300px] w-[650px] justify-between rounded-lg p-5'>
+            <div className='flex flex-col items-center bg-gray-200 shadow-xl hover:bg-gray-200 transition-all my-10 w-[550px] justify-between rounded-lg p-5'>
 
-                <h2 className='font-bold text-3xl'>{currentUser.email}</h2>
-                <h4 className='font-bold text-xl'>Time of create: {new Date(currentUser.date).toLocaleString()}</h4>
-                <h4 className='font-bold text-xl'>Time of login: {new Date(currentUser.dateLogin).toLocaleString()}</h4>
+                <h2 className='font-bold text-3xl my-6'>{currentUser.email}</h2>
+                <h4 className='font-bold text-xl my-4'>Time of create: {new Date(currentUser.date).toLocaleString()}</h4>
+                <h4 className='font-bold text-xl my-4'>Time of login: {new Date(currentUser.dateLogin).toLocaleString()}</h4>
 
             </div>
         </div>
