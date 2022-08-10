1c1358cb61233df20a8c7220b5cccf74460c7793
Alexander R <ru5nedug@mail.ru>
2022-08-10 18:20:28 +0300

    remember me

--- src/components/authorization/Login.jsx
+++ src/components/authorization/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Input from '../../utils/input/Input';
 import { useDispatch } from 'react-redux';
 import { login } from '../../actions/user';
+import style from './Login.module.css';
 
 
 const Login = () => {
@@ -15,15 +16,26 @@ const Login = () => {
         <div className='flex justify-center'>
 
             <div
-                className='flex flex-col items-center bg-gray-200 shadow-xl hover:bg-gray-200 transition-all my-10 h-[300px] w-[650px] justify-between rounded-lg p-5'>
+                className='flex flex-col items-center bg-gray-200 shadow-xl hover:bg-gray-200 transition-all my-10 w-[650px] justify-between rounded-lg p-5'>
                 <h2 className='font-bold text-3xl'>Log In!</h2>
 
                 <Input value={email} setValue={setEmail} type='text' placeholder='Email...' />
                 <Input value={password} setValue={setPassword} type='password' placeholder='Password...' />
 
-                <div><input type='checkbox' checked={checkbox} onChange={() => setCheckbox(!checkbox)} /> Save </div>
+                <div className={style.checkbox}>
+                    <input
+                        className={style.customCheckbox}
+                        type='checkbox'
+                        id='color'
+                        name='color'
+                        checked={checkbox}
+                        onChange={() => setCheckbox(!checkbox)}
+                    />
+                    <label htmlFor='color'>Remember Me</label>
+                </div>
 
-                <button className='py-2 w-[150px] rounded-lg bg-blue-300 m-4 hover:shadow-md hover:bg-blue-400'
+
+                <button className='py-2 w-[150px] rounded-lg bg-blue-300 mb-4 mt-6 hover:shadow-md hover:bg-blue-400'
                         onClick={() => dispatch(login(email, password, checkbox))}>Log In
                 </button>
             </div>
